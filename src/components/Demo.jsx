473f251a4960d3dcb70fda9dc654177d63899a01
Madina <madinatussupova0820@gmail.com>
2023-05-26 10:47:57 +0600

    add url input

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 function Demo() {
-	return (
-		<div>Demo</div>
-	)
+	const [article, setArticle] = useState({
+		url: '',
+		summary: '',
+	});
+
+	const handleSubmit = async(e) => {
+		e.preventDefault();
+		alert("Submitted")
+	}
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+				<form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+					<img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+
+					<input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({...article, url: e.target.value})} required className="url_input peer" />
+
+					<button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
+					⮭
+					</button>
+				</form>
+
+				{/* Browse URL History */}
+			</div>
+
+			{/* Display results */}
+    </section>
+  );
 }
 
-export default Demo
+export default Demo;
