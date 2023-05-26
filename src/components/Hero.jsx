0b07bfae5b796bebc556329cfa2eaeb4b780c3d6
Madina <madinatussupova0820@gmail.com>
2023-05-26 10:19:00 +0600

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { logo } from "../assets";
 
 function Hero() {
-	return (
-		<div>Hero</div>
-	)
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+			<nav className="flex justify-between items-center w-full mb-10 pt-3">
+				<img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+				<button type="button" onClick={() => window.open('https://github.com/madina0801/ai_summarizer', '_blank')} className="black_btn">
+					GitHub
+				</button>
+			</nav>
+
+			<h1 className="head_text">
+				Summarize Articles with <br className="max-md:hidden" />
+				<span className="orange_gradient">OpenAI GPT-4</span>
+			</h1>
+
+			<h2 className="desc">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, ad incidunt repellat esse earum velit praesentium vitae odio nisi minima distinctio commodi quod deserunt excepturi explicabo sit, assumenda illo quisquam!
+			</h2>
+		</header>
+  );
 }
 
-export default Hero
+export default Hero;
