7273d32b2a3e9a7bb69abbedc351fe5a6c020f44
Madina <madinatussupova0820@gmail.com>
2023-05-29 15:16:05 +0600

    add openai api

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\nTl;dr",
+  temperature: 0.7,
+  max_tokens: 60,
+  top_p: 1.0,
+  frequency_penalty: 0.0,
+  presence_penalty: 1,
+});
 
 export const articleApi = createApi({
 	reducerPath: 'articleApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: '',
+	}),
+	endpoints: (builder) => ({
+		getSummary: builder.query({
+			query: (params) => 'test'
+		})
+	})
 });
