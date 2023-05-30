dad3562dfe9643415346044bf3e1701a216f39a8
Madina <madinatussupova0820@gmail.com>
2023-05-30 10:29:59 +0600

    browse url history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -66,6 +66,17 @@ function Demo() {
         </form>
 
         {/* Browse URL History */}
+				<div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+					{allArticles.map((item, index) => (
+						<div key={`link-${index}`} onClick={() => setAtricle(item)} className="link_card">
+							<div className="copy_btn">
+								<img src={copy} alt="copy_icon" className="w-[40%] object-contain" />
+							</div>
+
+							<p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+						</div>
+					))}
+				</div>
       </div>
 
       {/* Display results */}
